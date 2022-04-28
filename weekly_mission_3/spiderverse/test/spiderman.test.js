79c79ad3d594c2696796848a19c4be5115913cc1
selene <seleneugs@gmail.com>
2022-04-27 23:26:06 -0500

    termiando la refactorizacion

--- weekly_mission_3/spiderverse/test/spiderman.test.js
+++ weekly_mission_3/spiderverse/test/spiderman.test.js
@@ -1,6 +1,17 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const Spiderman = require('./../app/spiderman')
+
+describe("Unit test for Spiderman class", () => {
+    test('(1) create a spiderman object', () => {
+      const andrew = new Spiderman("SpidermanSony", 31, "Andrew Garfield",2,"Sony");
+      
+      expect(andrew.name).toBe("SpidermanSony");
+      expect(andrew.age).toBe(31);
+      expect(andrew.actor).toBe("Andrew Garfield");
+      expect(andrew.movies).toBe(2);
+      expect(andrew.studio).toBe("Sony");
+    });
+    test('(2)Use the method getInfo()', () => {
+        const holland = new Spiderman("Spiderman Marvel",25,"Tom Holland",5,"Marvel")
+        expect(holland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
     });
   })
