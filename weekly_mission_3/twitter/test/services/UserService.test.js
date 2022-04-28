acfcc61b64571ee2d9abb108ad032d35a1f0a1be
selene <seleneugs@gmail.com>
2022-04-28 03:35:29 -0500

    primera prueba

---
+++ weekly_mission_3/twitter/test/services/UserService.test.js
@@ -0,0 +1,12 @@
+const UserService = require('./../../app/services/UserService')
+
+describe("Tests for UserService", () =>{
+    test("1. Create a new user using thw UserService", () =>{
+        const user = UserService.create(1,"carlogilmar","Carlo")
+
+        expect(user.username).toBe("carlogilmar");
+        expect(user.name).toBe("Carlo");
+        expect(user.id).toBe(1);
+        expect(user.bio).not.toBeUndefined()
+    })
+})
