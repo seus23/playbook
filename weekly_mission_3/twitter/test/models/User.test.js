e173b41f885ea49eeab40f06aa299fd69f6a22fb
selene <seleneugs@gmail.com>
2022-04-28 03:10:06 -0500

    requerimiento 3

--- weekly_mission_3/twitter/test/models/User.test.js
+++ weekly_mission_3/twitter/test/models/User.test.js
@@ -2,13 +2,21 @@ const User = require('./../../app/models/user')
 
 describe("Unit test for User class", () => {
     test('Create an User object',() =>{
-        const user = new User(1, "seus", "Selene", "Bio", "dateCreated", "lastUpdated")
+        const user = new User(1, "seus", "Selene", "Bio")
 
         expect(user.id).toBe(1);
         expect(user.username).toBe('seus');
         expect(user.name).toBe("Selene");
         expect(user.bio).toBe("Bio");
-        expect(user.dateCreated).toBe("dateCreated");
-        expect(user.lastUpdated).toBe("lastUpdated");
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
+    })
+
+    test('Add getters', () => {
+        const user = new User(1,"carlogilmar","carlo","Bio")
+        expect(user.getUsername).toBe("carlogilmar");
+        expect(user.getBio).toBe("Bio");
+        expect(user.getDateCreated).not.toBeUndefined();
+        expect(user.getLastUpdated).not.toBeUndefined();
     })
 })
