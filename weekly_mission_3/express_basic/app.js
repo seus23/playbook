7a7c3c86a996df9e9157dfe2adb4dc2b4b6c9adb
selene <seleneugs@gmail.com>
2022-04-29 03:11:54 -0500

    Mi primer proyecto con express, completo

--- weekly_mission_3/express_basic/app.js
+++ weekly_mission_3/express_basic/app.js
@@ -10,6 +10,26 @@ app.get('/',(req,res) =>{
     res.send('Hello World!, Khe sta pasanda?')
 })
 
+//respondiendo texto
+//localhost:3000/launchx
+app.get('/launchx', (req,res) => {
+    res.send('Bienvenidos a launchX')
+})
+
+//Regresando un objeto
+//localhost:3000/explorersInNode
+app.get('/explorersInNode', (req,res) => {
+    const explorer = {name:"Explorer", msg:"Hello"}
+    res.send(explorer)
+})
+//Query params: Recibir parametros por la url
+//http://localhost:3000/explorers/seus
+//req.params = {"explorerName":"seus"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
+
+
 //Con esto inicializamos esta app
 app.listen(port, () => {
     console.log('Example app listening on port ${port}')
