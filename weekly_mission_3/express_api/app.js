b1db7194bb1cd5160b3f09b4100b759937148dfe
selene <seleneugs@gmail.com>
2022-04-30 22:57:41 -0500

    correccion de texto

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -10,9 +10,9 @@ const port = 3000
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
     const explorer1 = {id: 1, name:"seus1"}
-    const explorer2 = {id: 2, name:"seus1"}
-    const explorer3 = {id: 3, name:"seus1"}
-    const explorer4 = {id: 4, name:"seus1"}
+    const explorer2 = {id: 2, name:"seus2"}
+    const explorer3 = {id: 3, name:"seus3"}
+    const explorer4 = {id: 4, name:"seus4"}
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 })
