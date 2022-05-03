6f4ae57a29b90507d04ee997ce84e3150f318642
selene <seleneugs@gmail.com>
2022-05-02 20:45:24 -0500

    api de lauchx

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -16,6 +16,38 @@ app.get('/v1/explorers', (req, res) => {
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 })
+
+//Endpoint que regresa un explorer
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET requests ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id: 1, name:"seus23"}
+    res.status(200).json(explorer)
+})
+
+//Endpoint que crea un explorer
+app.post('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //parametros de un cliente
+    res.status(201).json({message: "Created"})
+})
+
+//Endpoint que actualiza un explorer
+app.put('/v1/explorers/:id', (req,res) => {
+    console.log(`Api explorers PUT request ${new Date()}`)
+    console.log('Update explorer with id ${req.params.id}')
+    const requestBody = req.body //parametro de un cliente
+    res.status(200).json({message: "Updated!"})
+})
+
+//Endpoint que elimina un explorer
+app.delete('/v1/explorers/:id', (req,res) => {
+    console.log(`ApiExplorers DELETE request ${new Date()}`)
+    console.log(`Dñete explorer with id ${req.params.id}`)
+    const requestBody = req.body //parametro de un cliente
+    res.status(200).json({message: "Deleted"})
+})
+
 //con esto inicializamos esta app
 app.listen(port, () => {
     console.log(`Example app listening on por ${port}`)
