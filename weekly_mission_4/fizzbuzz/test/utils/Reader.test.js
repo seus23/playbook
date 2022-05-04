49232d079223124398c88abc56bab85dce3469f1
selene <seleneugs@gmail.com>
2022-05-04 10:33:30 -0500

    eliminando la carpeta fizzbuzz, se encuentra en un nuevo repo

--- weekly_mission_4/fizzbuzz/test/utils/Reader.test.js
+++
@@ -1,8 +0,0 @@
-const Reader = require('./../../lib/utils/Reader');
-
-describe("Unit Tests for Reader Class", () => {
-    test('1) lee el archivo', () => {
-        const result = 4+2;
-        expect(result).toBe(6);
-    });
-})
